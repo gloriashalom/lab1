63582124ad4432a28ef2bd7a0ba20d0fbdba015e
gloriashalom <74999645+gloriashalom@users.noreply.github.com>
2020-12-02 11:47:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,44 @@
-<html>
-<head>
-<title>Lab1 </title>
-</head>
-<body>
-<h1>Hello World</h1>
-</body>
-</html>
+<html>    
+ <head>          
+ <title>List of KL Restaurant</title>   
+ </head>   
+ <body>         
+ <center>            
+ <h1>List of KL Restaurant</h1>        
+ <!--create table structure using HTML first-->      
+ <table border="1">                
+ <tr>                   
+ <th>Restaurant ID</th>    
+ <th>Restaurant Name</th>    
+ <th>Address</th>            
+ <th>Phone</th>             
+ </tr>              
+ <tr>               
+ <th>0</th>        
+ <th>Subway Restaurant</th>    
+ <th>Menara Standard Chartered, TPM</th>       
+ <th>03-22441234</th>     
+ </tr>  
+
+<?php    
+    $serverName = "simplewebtp045200.database.windows.net";      
+	$connectionOptions = array(                           
+	"Database" => "simpleweb",                        
+	"Uid" => "apuadmin",                             
+	"PWD" => "#tp045200"); //Establishes the connection    
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+	if (!$conn) {            die("Error connection: ".sqlsrv_errors());        }  
+	$tsql= "SELECT * FROM [dbo].[restaurant]";     
+	$getResults= sqlsrv_query($conn, $tsql);  
+	if ($getResults == FALSE)  {             die(sqlsrv_errors());  }    
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))  
+	{             echo "<tr>";             echo "<td>". $row['restaurant_id'] . "</td>";   
+	echo "<td>". $row['restaurant_name'] ."</td>";          
+	echo "<td>". $row['restaurant_address'] . "</td>";          
+	echo "<td>". $row['restaurant_phone'] . "</td>";            
+	echo "</tr>";         }     
+	sqlsrv_free_stmt($getResults); ?> 
+ </table>        
+ </center>   
+ </body>
+ </html> 
